70dc139b3efbcff077ba1b648f35e499bcbc7a5b
yunabananas <r202202264ze@jindai.jp>
2022-11-02 10:54:11 +0900

    20221031

---
+++ de12/python/kabu.py
@@ -0,0 +1,17 @@
+
+name = int(input('1働き蜂の性別は？1～3のうちどれか一つ選んで入力してください?1,メス 2,オス 3,両方'))
+if name == 1:
+    print(name,"正解です")
+    
+elif name == 2:
+    print(name,"惜しい!!なんで間違えたか明日までに考えといてください!正解は1のメスです!")
+    
+elif name == 3:
+    print(name,"正解は1のメスです!")
+
+elif name == 4:
+    
+
+import webbrowser
+
+webbrowser.open('https://www.nikkei.com/nkd/industry/stocklist/?n_m_code=121')
---
+++ de12/python/kabu2.py
@@ -0,0 +1,3 @@
+import webbrowser
+
+webbrowser.open('https://www.nikkei.com/nkd/industry/stocklist/?n_m_code=121')
