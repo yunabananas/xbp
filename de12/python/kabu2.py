f615db093c89d6272f1788091f6c3bbe5c171c70
yunabananas <r202202264ze@jindai.jp>
2022-11-07 23:34:22 +0900

    20221107

--- de12/python/kabu2.py
+++ de12/python/kabu2.py
@@ -1,3 +1,11 @@
 import webbrowser
 
 webbrowser.open('https://www.nikkei.com/nkd/industry/stocklist/?n_m_code=121')
+#ウェブを開くためのもの
+
+a = ["stock.py", "systemsoftware.py", "retail.py", "finamce.py", "massmedia.py"]
+print(a["https://www.nikkei.com/nkd/industry/stocklist/?n_m_code=121"])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
